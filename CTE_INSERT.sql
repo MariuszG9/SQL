0d29f5b75738ebf5e0822a58f16b0d713a1800ab
MariuszG9 <120599403+MariuszG9@users.noreply.github.com>
2023-02-03 13:16:30 +0100

    Create CTE_INSERT.sql

---
+++ CTE_INSERT.sql
@@ -0,0 +1,20 @@
+WITH
+new_update AS 
+(
+   SELECT 
+      sCTE.inv_numb,
+      sCTE.qty,
+      sCTE.vendor_id,
+      sCTE.salvalue
+   FROM sales_cte sCTE
+   WHERE sCTE.vendor_id = '256562'
+)
+INSERT INTO sales_cte
+   (inv_numb, inv_date, vendor_id,qty, salvalue)
+SELECT
+   vnd.inv_numb,
+   GETDATE(),
+   '256562’,
+   vnd.qty,
+   (vnd.salvalue * 1.1 )
+FROM new_update vnd
