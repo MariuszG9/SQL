ffca05a5f34d67854a3abc2b87d20ca98796e9a6
MariuszG9 <120599403+MariuszG9@users.noreply.github.com>
2023-02-08 16:29:39 +0100

    Update OVER_PART_qty_sal.sql

--- OVER_PART_qty_sal.sql
+++ OVER_PART_qty_sal.sql
@@ -7,7 +7,7 @@ GROUP BY vendor_id;
 
 
 SELECT 
-   vendor_id,
-   SUM(qty) OVER(PARTITION BY vendor_id) AS quantity_summarry,
-   SUM(salvalue) OVER(PARTITION BY vendor_id)  AS sales_summarry
-FROM sales_both;
+	DISTINCT(vendor_id) AS vendor_id,
+	SUM(qty) OVER(PARTITION by vendor_id) AS quantity_summarry,
+	SUM(salvalue) OVER(PARTITION by vendor_id)  AS sales_summarry
+ FROM sales_both;
